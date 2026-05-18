f909ff6538c1baeb351df637b77e02adf3eead80
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-05-18 09:40:08 +0330

    ui: minor change

--- src/components/PomodoroTimerCreate.jsx
+++ src/components/PomodoroTimerCreate.jsx
@@ -2,7 +2,7 @@ import PomodoroTimerInput from "./PomodoroTimerInput";
 
 export default function PomodoroTimerCreate() {
   return (
-    <div className="flex-center h-full w-full flex-col gap-1.5 rounded-2xl border border-white p-2 text-base font-semibold text-black/70">
+    <div className="flex-center h-full w-full flex-col gap-1.5 rounded-2xl border border-white p-2 text-base font-semibold text-black/70 bg-white/20">
       <div className="flex-center w-full gap-1.5">
         <div className="form-input-pomodoro flex-center w-1/5 text-black/40">
           1
