bbaadd88ecadfd2c7ce682b1bdfd72e9ba4c120f
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-05-19 11:59:08 +0330

    ui: edit pomodoro style

--- src/components/PomodoroTimerCreate.jsx
+++ src/components/PomodoroTimerCreate.jsx
@@ -4,6 +4,10 @@ export default function PomodoroTimerCreate() {
   return (
     <div className="flex-center h-full w-full flex-col gap-1.5 rounded-2xl border border-white bg-white/20 p-2 text-sm font-semibold text-black/70">
       <div className="flex-center w-full gap-1.5">
+        <div className="bg-background flex-center aspect-square h-8 gap-1 rounded-2xl border border-white text-center text-xs text-black/40">
+          1
+        </div>
+
         <div className="form-container-pomodoro">
           <span>Work:</span>
           <PomodoroTimerInput value="25" />
@@ -16,7 +20,12 @@ export default function PomodoroTimerCreate() {
           <span>min</span>
         </div>
       </div>
+
       <div className="flex-center w-full gap-1.5">
+        <div className="bg-background flex-center aspect-square h-8 gap-1 rounded-2xl border border-white text-center text-xs text-black/40">
+          2
+        </div>
+
         <div className="form-container-pomodoro">
           <span>Work:</span>
           <PomodoroTimerInput value="25" />
@@ -29,7 +38,12 @@ export default function PomodoroTimerCreate() {
           <span>min</span>
         </div>
       </div>
+
       <div className="flex-center w-full gap-1.5">
+        <div className="bg-background flex-center aspect-square h-8 gap-1 rounded-2xl border border-white text-center text-xs text-black/40">
+          3
+        </div>
+
         <div className="form-container-pomodoro">
           <span>Work:</span>
           <PomodoroTimerInput value="25" />
@@ -42,7 +56,12 @@ export default function PomodoroTimerCreate() {
           <span>min</span>
         </div>
       </div>
+
       <div className="flex-center w-full gap-1.5">
+        <div className="bg-background flex-center aspect-square h-8 gap-1 rounded-2xl border border-white text-center text-xs text-black/40">
+          4
+        </div>
+
         <div className="form-container-pomodoro">
           <span>Work:</span>
           <PomodoroTimerInput value="25" />
