a7cacb3c4d4e4463cf8c2a70eb124cf2e6866125
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-06-13 19:23:30 +0330

    ui: add icon

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -5,7 +5,7 @@ export default function Warning({ children }) {
     <div className="flex-center focus-primary transition-animation min-h-0 shrink-0 scale-100 gap-x-1.5 p-2 text-xs opacity-100 h-fit w-full rounded-xl bg-red-500/80 text-white dark:text-dark-theme-text">
       <div className="w-full px-2 py-1">
         <span className="flex-center justify-start gap-1.5">
-          <Icon name="info" color="white" size="18" />
+          <Icon name="warning" color="white" />
           <h2 className="text-sm font-bold">Warning</h2>
         </span>
         <hr className="mt-1 mb-2 border-white/25" />
