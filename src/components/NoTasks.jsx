d1b0d06a8c77fc4c4bf672e7ffa912c046093107
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-06-14 11:49:16 +0330

    fix: typo

--- src/components/NoTasks.jsx
+++ src/components/NoTasks.jsx
@@ -5,10 +5,12 @@ export default function NoTasks() {
   return (
     <div className="flex-center h-full w-full">
       <div className="flex-center w-fit flex-col gap-3 rounded-2xl px-4 py-6">
-        <span className="enter-transition text-sm text- dark:text-dark-theme-text">No Tasks</span>
+        <span className="enter-transition text- dark:text-dark-theme-text text-sm">
+          No Tasks
+        </span>
         <Button type="capsuleAddTask" width="fit" actionType="toggleCreateTask">
           <Icon name="plus" color="white" size="14" />
-          <span clssName="text-md">Create a New Task</span>
+          <span>Create a New Task</span>
         </Button>
       </div>
     </div>
