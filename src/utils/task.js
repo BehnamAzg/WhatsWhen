5b58491718be19151c83cfb508e8719dd0c3cd2b
Behnam <110087586+BehnamAzg@users.noreply.github.com>
2026-06-17 21:47:07 +0330

    fix: crypto.randomUUID() was not supported on mobile browser

--- src/utils/task.js
+++ src/utils/task.js
@@ -3,7 +3,11 @@ import { formatTime } from "./time";
 function normalizeTask(task, viewDate) {
   return {
     ...task,
-    id: task.id || crypto.randomUUID(),
+    id:
+      task.id ||
+      (crypto?.randomUUID
+        ? crypto.randomUUID()
+        : Date.now().toString() + Math.random().toString(36).slice(2)),
     time: task.time || formatTime(new Date()),
     title: task.title || "Untitled",
     date: task.date || viewDate,
